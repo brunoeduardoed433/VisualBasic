348f61a0274e6d2a23c7de7386a93804e6d627e5
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2022-09-06 17:07:17 -0300

    Temperatura_GPU

---
+++ Programadores/VB (Temperatura GPU)/VisualBasic2/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Console.WriteLine(vbCrLf + "Temperatura da GPU (°C): ")
+        Dim temp = Console.ReadLine()
+
+        If temp <= 30 Then
+            Console.WriteLine("Temperatura Normal")
+
+        ElseIf temp <= 50 Then
+            Console.WriteLine("Temperatura Alta")
+
+        Else
+            Console.WriteLine("CUIDADO! Temperatura Acima Do Limite")
+        End If
+
+    End Sub
+End Module
--- Programadores/VisualBasic (Hello World)/Program.vb
+++ Programadores/VisualBasic (Hello World)/Program.vb
@@ -1,6 +1,8 @@
 Imports System
 
 Module Program
-    Dim var1 As String = "Hello "
+    Dim var1 As String = "Hello"
     Dim var2 As String = "World!"
+
+
 End Module
